6842dfd2f79a69550d0341fb36731b32df60c617
Patrick Strateman <patrick.strateman@gmail.com>
2020-08-24 15:28:03 -0400

    Add url field to cameras table.

--- postgresql.sql
+++ postgresql.sql
@@ -1,6 +1,6 @@
 BEGIN;
 CREATE TABLE sensors(id uuid PRIMARY KEY, label text);
-CREATE TABLE cameras(id uuid PRIMARY KEY REFERENCES sensors(id), location text);
+CREATE TABLE cameras(id uuid PRIMARY KEY REFERENCES sensors(id), url text not null, location text);
 CREATE TABLE segments(id uuid PRIMARY KEY, camera_id uuid REFERENCES cameras(id), start timestamp with time zone, stop timestamp with time zone);
 CREATE TABLE events(id uuid PRIMARY KEY, sensor_id uuid REFERENCES sensors(id), start timestamp with time zone, stop timestamp with time zone);
 COMMIT;
