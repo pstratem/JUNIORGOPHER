0dab239deee715bba020fdf2d8e170cb076e6334
Patrick Strateman <patrick.strateman@gmail.com>
2020-08-26 15:09:51 -0400

    a

--- live_monitor.py
+++ live_monitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import cv2 as cv
+import numpy as np
 import psycopg2, multiprocessing, time, os.path
 
 def monitor_camera(camera_id, camera_url):
@@ -24,17 +25,9 @@ def monitor_camera(camera_id, camera_url):
         
         if (capture.get(cv.CAP_PROP_POS_FRAMES) % (frame_rate / 5)) == 0:
             foreground_mask = background_subtractor.apply(frame)
-            
-            output_frame = foreground_mask
-            
             ret, threshold_image = cv.threshold(foreground_mask, 150, 255, cv.THRESH_BINARY)
-            
-            output_frame = cv.hconcat([output_frame, threshold_image])
-            
             contours, hierarchy = cv.findContours(threshold_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-            
-            contour_image = frame
-            # loop over the contours
+            contour_image = np.copy(frame)
             contours_written = 0
             for contour in contours:
                 # if the contour is too small, ignore it
@@ -48,9 +41,7 @@ def monitor_camera(camera_id, camera_url):
                 contour_image = cv.putText(contour_image, str(contour_area), (x, y), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
                 contours_written += 1
             if contours_written > 0:
-                print(output_frame.scale, contour_image.scale)
-                output_frame = cv.hconcat([output_frame, contour_image])
-                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(frame_time) + ".jpg"), frame)
+                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + ".jpg"), cv.hconcat([frame, foreground_mask, threshold_image, contour_image]))
 
         if False:
             db = psycopg2.connect(dbname="juniorgopher")
