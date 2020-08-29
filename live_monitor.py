382f6346df4f022e6956d1747672d41be758d08a
Patrick Strateman <patrick.strateman@gmail.com>
2020-08-28 21:14:28 -0400

    switch to despeckler

--- live_monitor.py
+++ live_monitor.py
@@ -3,6 +3,12 @@ import cv2 as cv
 import numpy as np
 import psycopg2, multiprocessing, time, os.path
 
+def despeckle(threshold_image):
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (threshold_image.shape[0] * 0.003, thresold_image.shape[1] * 0.003))
+    dilate_image = cv.dilate(threshold_image, kernel)
+    erode_image = cv.erode(dilate_image, kernel)
+    return erode_image
+
 def monitor_camera(camera_id, camera_url):
     background_subtractor = cv.createBackgroundSubtractorMOG2()
     capture = cv.VideoCapture(camera_url)
@@ -25,25 +31,13 @@ def monitor_camera(camera_id, camera_url):
         
         if (capture.get(cv.CAP_PROP_POS_FRAMES) % (frame_rate / 5)) == 0:
             foreground_mask = background_subtractor.apply(frame)
-            ret, threshold_image = cv.threshold(foreground_mask, 150, 255, cv.THRESH_BINARY)
-            contours, hierarchy = cv.findContours(threshold_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-            contour_image = np.copy(frame)
-            contours_written = 0
-            for contour in contours:
-                # if the contour is too small, ignore it
-                contour_area = cv.contourArea(contour)
-                if contour_area < (threshold_image.shape[0] * threshold_image.shape[1]) * 0.005:
-                    continue
-#                (x, y, w, h) = cv.boundingRect(contour)
-#                contour_image = cv.rectangle(contour_image, (x, y), (x + w, y + h), (0, 255, 0))
-                contour_image = cv.drawContours(contour_image, [contour,], 0, (0,0,255), 1)
-#                contour_image = cv.putText(contour_image, str(contour_area), (x, y), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
-                contours_written += 1
-            if contours_written > 0:
-                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "frame" + ".jpg"), frame)
-                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "mask" + ".jpg"), foreground_mask)
-                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "threshold" + ".jpg"), threshold_image)
-                retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "contour" + ".jpg"), contour_image)
+            ret, threshold_image = cv.threshold(foreground_mask, 255, 255, cv.THRESH_BINARY)
+            despeckled_image = despeckle(threshold_image)
+            
+            retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "frame" + ".jpg"), frame)
+            retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "mask" + ".jpg"), foreground_mask)
+            retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "threshold" + ".jpg"), threshold_image)
+            retval = cv.imwrite(os.path.join(camera_fgmasks_path, str(int(frame_time*1000)) + "despeckle" + ".jpg"), despeckled_image)
                 
 
         if False:
