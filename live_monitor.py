f798221be7053a36bf46db7375c4cbb087fe422a
Patrick Strateman <patrick.strateman@gmail.com>
2020-08-31 13:08:30 -0400

    a

--- live_monitor.py
+++ live_monitor.py
@@ -4,9 +4,9 @@ import numpy as np
 import psycopg2, multiprocessing, time, os.path
 
 def despeckle(threshold_image):
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (int(threshold_image.shape[0] * 0.01), int(threshold_image.shape[1] * 0.01)))
-    dilate_image = cv.dilate(threshold_image, kernel)
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (int(threshold_image.shape[0] * 0.003), int(threshold_image.shape[1] * 0.003)))
     erode_image = cv.erode(dilate_image, kernel)
+    dilate_image = cv.dilate(threshold_image, kernel)
     return erode_image
 
 def monitor_camera(camera_id, camera_url):
