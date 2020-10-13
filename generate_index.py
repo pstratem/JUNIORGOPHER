f4d1c9d35d77c1d5fade8244c14d502649f8435e
Patrick Strateman <patrick.strateman@gmail.com>
2020-10-13 17:46:14 -0400

    a

--- generate_index.py
+++ generate_index.py
@@ -4,8 +4,8 @@ import psycopg2, os, os.path, jinja2
 db = psycopg2.connect(dbname="juniorgopher")
 
 c = db.cursor()
-c.execute("SELECT id FROM cameras")
-camera_ids = c.fetchall()
+c.execute("SELECT id, url FROM cameras")
+camera_ids = [camera_id for camera_id, camera_url in c.fetchall()]
 
 template = jinja2.Template(open('index.html.jinga').read())
 
