78ee91b9353738b61fe5dd9d85894ca4e2148c83
Patrick Strateman <patrick.strateman@gmail.com>
2020-10-13 17:42:35 -0400

    move template to file

--- generate_index.py
+++ generate_index.py
@@ -7,35 +7,6 @@ c = db.cursor()
 c.execute("SELECT id FROM cameras")
 camera_ids = c.fetchall()
 
-template = jinja2.Template("""<html>
-    <head>
-        <title>Cameras</title>
-        <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-    </head>
-    <body>
-      {% for camera_id in camera_ids %}
-          <video name="camera_feed" controls autoplay muted id="{{ camera_id }}" src="{{ camera_id }}.m3u8" />
-      {% endfor %}
-<script>
-  function fixCameraFeed(video) {
-      if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(video.src);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function() {
-          video.play();
-        });
-      }
-      else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.addEventListener('loadedmetadata', function() {
-          video.play();
-        });
-      }
-  }
-  var camera_feeds = document.getElementsByName('camera_feed')
-  camera_feeds.forEach(fixCameraFeed)
-</script>
-    </body>
-</html>""")
+template = jinja2.Template(open('index.html.jinga').read())
 
 print(template.render(camera_ids=camera_ids))
