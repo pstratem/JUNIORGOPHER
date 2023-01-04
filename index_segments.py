d2ed4b005fe84a6b2b423d0ade720b0be8cdb7f1
Patrick Strateman <patrick.strateman@gmail.com>
2023-01-03 23:05:12 -0500

    add day_start

--- index_segments.py
+++ index_segments.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import psycopg, os, os.path, jinja2, re
+import psycopg, os, os.path, jinja2, re, datetime
 
 segment_pattern = re.compile('^(\d+).mp4$')
 day_pattern = re.compile('^(\d+)$')
@@ -18,30 +18,26 @@ for camera_id, camera_url in cameras:
 # move segments into hierarchy to improve performance
 for camera_id, camera_url in cameras:
     camera_segment_path = F"/var/lib/juniorgopher/segments/{camera_id}"
-    for dirpath, dirnames, filenames in os.walk(camera_segment_path):
-        if dirpath == camera_segment_path:
-            for filename in filenames:
-                match = segment_pattern.match(filename)
-                if not match:
-                    continue
-                timestamp = int(match.group(1))
-                timestamp_truncated_to_day = timestamp - (timestamp % (3600 * 24))
-                daily_timestamp_dirname = os.path.join(camera_segment_path, str(timestamp_truncated_to_day))
-                os.makedirs(daily_timestamp_dirname, exist_ok=True)
-                segment_path = os.path.join(dirpath, filename)
-                new_segment_path = os.path.join(daily_timestamp_dirname, filename)
-                os.rename(segment_path, new_segment_path)
+    for entry in os.scandir(camera_segment_path):
+        segment_match = segment_pattern.match(entry.name)
+        if entry.is_file() and segment_match:
+            timestamp = int(segment_match.group(1))
+            timestamp_truncated_to_day = timestamp - (timestamp % (3600 * 24))
+            daily_timestamp_dirname = os.path.join(camera_segment_path, str(timestamp_truncated_to_day))
+            os.makedirs(daily_timestamp_dirname, exist_ok=True)
+            segment_path = os.path.join(camera_segment_path, entry.name)
+            new_segment_path = os.path.join(daily_timestamp_dirname, entry.name)
+            os.rename(segment_path, new_segment_path)
 
 # insert into database
 for camera_id, camera_url in cameras:
     camera_segment_path = F"/var/lib/juniorgopher/segments/{camera_id}"
     for day_entry in os.scandir(camera_segment_path):
-        if day_entry.is_dir() and day_pattern.match(day_entry.name):
+        day_match = day_pattern.match(day_entry.name)
+        if day_entry.is_dir() and day_match:
             for segment_entry in os.scandir(os.path.join(camera_segment_path, day_entry.name)):
                 segment_match = segment_pattern.match(segment_entry.name)
                 if segment_entry.is_file() and segment_match:
                     relative_path = os.path.join(day_entry.name, segment_entry.name)
-                    c.execute("INSERT INTO segments(id, camera_id, start, relative_path) VALUES (gen_random_uuid(), %s, %s, %s) ON CONFLICT DO NOTHING",
-                        (camera_id, datetime.datetime.utcfromtimestamp(int(segment_match.group(1))), relative_path))
-
+                    c.execute("INSERT INTO segments(id, camera_id, start, relative_path, day_start) VALUES (gen_random_uuid(), %s, %s, %s, %s) ON CONFLICT (camera_id, relative_path) DO UPDATE SET relative_path=EXCLUDED.relative_path, day_start=EXCLUDED.day_start", (camera_id, datetime.datetime.utcfromtimestamp(int(segment_match.group(1))), relative_path, datetime.datetime.utcfromtimestamp(int(day_match.group(1)))))
 db.commit()
--- postgresql.sql
+++ postgresql.sql
@@ -1,6 +1,6 @@
 BEGIN;
 CREATE TABLE sensors(id uuid PRIMARY KEY, label text);
 CREATE TABLE cameras(id uuid PRIMARY KEY REFERENCES sensors(id), url text not null, location text);
-CREATE TABLE segments(id uuid PRIMARY KEY, camera_id uuid REFERENCES cameras(id), start timestamp with time zone, stop timestamp with time zone);
+CREATE TABLE segments(id uuid PRIMARY KEY, camera_id uuid REFERENCES cameras(id), start timestamp with time zone, stop timestamp with time zone, relative_path character varying, day_start timestamp with time zone);
 CREATE TABLE events(id uuid PRIMARY KEY, sensor_id uuid REFERENCES sensors(id), start timestamp with time zone, stop timestamp with time zone);
 COMMIT;
